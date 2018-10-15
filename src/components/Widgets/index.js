5e414ed541f9e046a2d9037d0a0773717a837c2a
woothu <woothu@gmail.com>
2018-10-15 15:14:17 +0200

    feat: added simple charts components and change naming of widgets

---
+++ src/components/Widgets/index.js
@@ -0,0 +1,12 @@
+import CuiWidget01 from './CuiWidget01'
+import CuiWidget02 from './CuiWidget02'
+import CuiWidget03 from './CuiWidget03'
+import CuiWidget04 from './CuiWidget04'
+import CuiWidget05 from './CuiWidget05'
+import CuiWidget06 from './CuiWidget06'
+import CuiWidget07 from './CuiWidget07'
+import CuiWidget08 from './CuiWidget08'
+
+export {
+  CuiWidget01, CuiWidget02, CuiWidget03, CuiWidget04, CuiWidget05, CuiWidget06, CuiWidget07, CuiWidget08
+}
