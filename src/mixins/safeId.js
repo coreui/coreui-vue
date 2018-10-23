8b365cd3c2b24ea49273c94bc052f1d32d115adb
woothu <woothu@gmail.com>
2018-10-23 14:25:24 +0200

    Feat: Added safeId mixin

---
+++ src/mixins/safeId.js
@@ -0,0 +1,14 @@
+export default{
+  id: {
+    type: String,
+    default: null
+  },
+  computed: {
+    safeId () {
+      if(this.id)
+        return this.id
+
+      return Math.random().toString(36).replace('0.', '') + (new Date()).getTime().toString(36)
+    }
+  }
+}
