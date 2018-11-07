1b9de5aa8d8236304a646991caba5443031b891b
woothu <32914662+woothu@users.noreply.github.com>
2018-11-07 12:23:43 +0100

    fix: IE11 fix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+module.exports = { "presets": [
+    ["vue-app", {
+      "modules": false
+    }]
+  ],
+  "plugins": [
+    ["module-resolver", {
+      "extensions": [".js", ".vue", ".json"]
+    }]
+  ],
+  "env": {
+    "test": {
+      "plugins": ["dynamic-import-node"]
+    }
+  }
 }
