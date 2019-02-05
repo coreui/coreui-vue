bb122be1fd93f973bf4e2c7cc96d464c9a63bf62
woothu <32914662+woothu@users.noreply.github.com>
2019-02-05 12:15:55 +0100

    feat: add generateSafeId funtion.

--- src/mixins/safeId.js
+++ src/mixins/safeId.js
@@ -1,13 +1,15 @@
 export default{
-  id: {
-    type: String,
-    default: null
-  },
   computed: {
     safeId () {
-      if(this.id)
-        return this.id
-      return Math.random().toString(36).replace('0.', '') + (new Date()).getTime().toString(36)
+      if(this.id || this.$attrs.id)
+        return this.id || this.$attrs.id
+      return generateSafeId()
     }
   }
+}
+
+function generateSafeId () {
+  const key = Math.random().toString(36).replace('0.', '') +
+              (new Date()).getTime().toString(36)
+  return '__safe_id__' + key
 }
