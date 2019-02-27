7098ad301d3ea3f0e12eb1478ed0a2d869bda4b7
woothu <32914662+woothu@users.noreply.github.com>
2019-02-27 16:07:39 +0100

    feat: changes in style, and pagination mechanism Style of sorting arrow changed to absolute to avoid jumping to next row Improved code readability according to vue style guide

--- src/components/Table/CTable.vue
+++ src/components/Table/CTable.vue
@@ -1,51 +1,56 @@
 <template>
   <div>
     <div v-if="optionsRow" class="row my-2 mx-0">
-      <div v-show="optionsRow !== 'onlyPagination'" class="col-sm-6 form-inline p-0">
+
+      <div
+        v-show="optionsRow !== 'onlyPagination'"
+        class="col-sm-6 form-inline p-0"
+      >
         <label class="mr-2">Filter: </label>
-        <input class="form-control c-table-filter"
-               type="text"
-               placeholder="type string..."
-               @input="tableFilter = $event.target.value"
-               :value="tableFilter"
+        <input
+          class="form-control c-table-filter"
+          type="text"
+          placeholder="type string..."
+          @input="tableFilter = $event.target.value"
+          :value="tableFilter"
         >
       </div>
+
       <div v-show="optionsRow !== 'onlyFilter'" class="col-sm-6 p-0">
         <div :class="`form-inline ${optionsRow === 'onlyPagination' ? '' : 'float-sm-right'}`">
           <label class="mr-2">Items per page: </label>
-          <input v-if="false"
-                 class="form-control"
-                 type="number"
-                 placeholder="type number..."
-                 @input="perPageItems = Number($event.target.value)"
+          <select
+            class="form-control"
+            @change="perPageItems = Number($event.target.value)"
           >
-          <select v-if="true"
-                  class="form-control"
-                  @change="perPageItems = Number($event.target.value)"
-          >
-            <option value="" selected disabled hidden>{{perPageItems}}</option>
-            <option v-for="number in [5,10,20,50]" val="number">{{number}}</option>
+            <option value="" selected disabled hidden>
+              {{perPageItems}}
+            </option>
+            <option v-for="number in [5,10,20,50]" val="number">
+              {{number}}
+            </option>
           </select>
         </div>
       </div>
     </div>
+
+
     <slot name="overTable"/>
-    <div :class="notResponsive ? '' : 'table-responsive'" class="position-relative">
+    <div :class="`position-relative ${notResponsive ? '' : 'table-responsive'}`">
       <table :class="tableClasses">
         <thead :class="headVariant ? `thead-${headVariant}` : ''">
+
           <tr>
-            <th v-if="indexCol" style="width:40px"></th>
+            <th v-if="indexColumn" style="width:40px"></th>
             <template v-for="(name, index) in columnNames">
               <th
                 @click="changeSort(rawColumnNames[index], index)"
-                :class="[headerClass(index), iconStyles]"
+                :class="[headerClass(index), sortingIconStyles]"
                 :style="headerStyles(index)"
               >
-                <slot v-if="$slots[`${rawColumnNames[index]}-header`]"
-                      :name="`${rawColumnNames[index]}-header`"
-                >
+                <slot :name="`${rawColumnNames[index]}-header`">
+                  <div v-html="name" class="d-inline"></div>
                 </slot>
-                <div v-else v-html="name" class="d-inline"></div>
                 <slot
                   v-if="!noSorting && sortable(index)"
                   name="sorting-icon"
@@ -56,63 +61,82 @@
               </th>
             </template>
           </tr>
+
           <tr v-if="filterRow" class="table-sm">
-            <th v-if="indexCol" class="pb-2">
-              <i v-if="indexCol !== 'onlyIndexes'" class="cui-ban icons text-danger font-lg text-center d-block" @click="clear" title="clear table"></i>
+            <th v-if="indexColumn" class="pb-2">
+              <i
+                v-if="indexColumn !== 'onlyIndexes'"
+                class="cui-ban icons text-danger font-lg text-center d-block"
+                @click="clear"
+                title="clear table"
+              ></i>
             </th>
             <template v-for="(colName, index) in rawColumnNames" >
               <th :class="headerClass(index)">
-                <slot :clear="clear, colName, index"
-                      :name="`${rawColumnNames[index]}-filter`"
+                <slot
+                  :clear="clear, colName, index"
+                  :name="`${rawColumnNames[index]}-filter`"
                 >
-                  <input v-if="!fields || !fields[index].noFilter"
-                         class="w-100 c-table-filter"
-                         @input="addColumnFilter(colName, $event.target.value)"
-                         :value="columnFilter[colName]"
+                  <input
+                    v-if="!fields || !fields[index].noFilter"
+                    class="w-100 c-table-filter"
+                    @input="addColumnFilter(colName, $event.target.value)"
+                    :value="columnFilter[colName]"
                   ></input>
                 </slot>
               </th>
             </template>
           </tr>
         </thead>
+
+
         <tbody :style="bodyStyle" class="position-relative">
           <template v-for="(item, itemIndex) in currentItems" >
-            <tr :class="item._classes" :tabindex="bodyStyle ? 0 : null"
-                @click="rowClicked(item, itemIndex + firstItemIndex)">
-                <slot v-if="indexCol"
-                      name="index-col"
-                      :index="firstItemIndex + itemIndex"
+            <tr
+              :class="item._classes" :tabindex="bodyStyle ? 0 : null"
+              @click="rowClicked(item, itemIndex + firstItemIndex)"
+            >
+                <slot
+                  v-if="indexColumn"
+                  name="index-col"
+                  :index="firstItemIndex + itemIndex"
                 >
                   <td>
-                    {{indexCol !== 'onlyCleaner' ? firstItemIndex + itemIndex + 1 : ''}}
+                    {{indexColumn !== 'onlyCleaner' ? firstItemIndex + itemIndex + 1 : ''}}
                   </td>
                 </slot>
               <template v-for="(colName, index) in rawColumnNames" >
-                <slot v-if="$scopedSlots[colName]"
-                      :name="colName"
-                      :item="item"
-                      :index="itemIndex + firstItemIndex"
+                <slot
+                  v-if="$scopedSlots[colName]"
+                  :name="colName"
+                  :item="item"
+                  :index="itemIndex + firstItemIndex"
                 />
-                <td v-else
-                    :class="cellClass(item, colName, index)"
-                    v-html="item[colName]"
+                <td
+                  v-else
+                  :class="cellClass(item, colName, index)"
+                  v-html="item[colName]"
                 ></td>
               </template>
             </tr>
-            <slot v-if="$scopedSlots.details || $scopedSlots.detailsRow"
-                  name="detailsRow"
-                  :item="item"
-                  :index="itemIndex  + firstItemIndex"
-                  :colspan="colspan">
+            <slot
+              v-if="$scopedSlots.details || $scopedSlots.detailsRow"
+              name="detailsRow"
+              :item="item"
+              :index="itemIndex  + firstItemIndex"
+              :colspan="colspan"
+            >
               <tr class="p-0" style="border:none !important">
-                <td :colspan="colspan"
-                    class="p-0"
-                    style="border:none !important"
+                <td
+                  :colspan="colspan"
+                  class="p-0"
+                  style="border:none !important"
                 >
-                  <slot name="details"
-                        :item="item"
-                        :index="itemIndex + firstItemIndex"
-                        :colspan="colspan"
+                  <slot
+                    name="details"
+                    :item="item"
+                    :index="itemIndex + firstItemIndex"
+                    :colspan="colspan"
                   />
                 </td>
               </tr>
@@ -123,26 +147,31 @@
               <slot name="empty-table">
                 <div class="text-center my-5">
                   <h2>{{ passedItems.length ? 'No filtering results ' : 'No items'}}
-                    <i style="font-weight:1000" class="text-danger icons font-2xl cui-ban"></i>
+                    <i
+                      style="font-weight:1000"
+                      class="text-danger icons
+                      font-2xl cui-ban"
+                    ></i>
                   </h2>
                 </div>
               </slot>
             </td>
           </tr>
         </tbody>
+
+
         <tfoot v-if="footer && currentItems.length > 3">
           <tr>
-            <th v-if="indexCol" style="width:40px"></th>
+            <th v-if="indexColumn" style="width:40px"></th>
             <template v-for="(name, index) in columnNames">
-              <th @click="changeSort(rawColumnNames[index], index)"
-                  :class="[headerClass(index), iconStyles]"
-                  :style="headerStyles(index)"
+              <th
+                @click="changeSort(rawColumnNames[index], index)"
+                :class="[headerClass(index), sortingIconStyles]"
+                :style="headerStyles(index)"
               >
-                <slot v-if="$slots[`${rawColumnNames[index]}-header`]"
-                      :name="`${rawColumnNames[index]}-header`"
-                >
+                <slot :name="`${rawColumnNames[index]}-header`">
+                  <div v-html="name" class="d-inline"></div>
                 </slot>
-                <div v-else v-html="name" class="d-inline"></div>
                 <slot
                   v-if="!noSorting && sortable(index)"
                   name="sorting-icon"
@@ -156,17 +185,29 @@
         </tfoot>
         <slot name="caption"/>
       </table>
-      <div v-if="loading" :style="topLoadingPosition" style="position:absolute;left:50%;transform:translateX(-50%);">
-        <div class="spinner-border text-success" :style="spinnerSize" role="status">
+
+
+      <div
+        v-if="loading"
+        :style="topLoadingPosition"
+        style="position:absolute;left:50%;transform:translateX(-50%);"
+      >
+        <div
+          class="spinner-border text-success"
+          :style="spinnerSize"
+          role="status"
+        >
           <span class="sr-only">Loading...</span>
         </div>
       </div>
     </div>
     <slot name="underTable"/>
+
+
     <CPagination v-if="!noPagination"
-                 v-show="pages > 1"
+                 v-show="totalPages > 1"
                  v-model="page"
-                 :pages="pages"
+                 :pages="totalPages"
                  v-bind="paginationProps"
     />
   </div>
@@ -188,7 +229,7 @@ export default {
       default: 10
     },
     activePage: Number,
-    indexCol: [Boolean, String],
+    indexColumn: [Boolean, String],
     filterRow: Boolean,
     noPagination: Boolean,
     paginationProps: Object,
@@ -224,7 +265,6 @@ export default {
       sorter: { name: this.defaultSorter.name, direction: this.defaultSorter.direction },
       firstItemIndex: 0,
       page: this.activePage || 1,
-      pages: 0,
       perPageItems: this.perPage,
       passedItems: this.items
     }
@@ -263,11 +303,13 @@ export default {
     },
     currentItems () {
       if (this.computedPage) {
-        this.checkPagination()
         this.firstItemIndex = (this.computedPage - 1) * this.perPageItems
         return this.sortedItems.slice(this.firstItemIndex, this.firstItemIndex + this.perPageItems)
       }
       return this.sortedItems
+    },
+    totalPages () {
+      return Math.ceil((this.sortedItems.length)/ this.perPageItems) || 1
     },
     computedPage () {
       return this.noPagination ? this.activePage : this.page
@@ -307,8 +349,11 @@ export default {
     bodyStyle () {
       return this.$listeners && this.$listeners['row-clicked'] ? 'cursor:pointer' : ''
     },
+    sortingIconStyles () {
+      return !this.noSorting ? 'position-relative pr-4' : ''
+    },
     colspan () {
-      return this.indexCol ? this.rawColumnNames.length + 1 : this.rawColumnNames.length
+      return this.indexColumn ? this.rawColumnNames.length + 1 : this.rawColumnNames.length
     },
     topLoadingPosition () {
       const headerHeight = (this.filterRow ? 38 : 0) + ( this.small ? 32 + 4 : 46 + 7)
@@ -318,32 +363,22 @@ export default {
       const size = this.small ? '1.4rem' : this.currentItems.length === 1 ? '2rem' : '3rem'
       return `width:${size};height:${size}`
     },
-    iconStyles () {
-      return !this.noSorting ? 'position-relative pr-4' : ''
-    }
-    // loadingStyles () {
-    //   return !this.loading ? '' :
-    //     `opacity: .4; ${this.loading === 'noEvents' ? 'pointer-events:none' : ''}`
-    // }
+
   },
   watch: {
-    // filter () { deep:true },
     items (val, oldVal) {
       if(val.length !== oldVal.length || JSON.stringify(val) !== JSON.stringify(oldVal))
         this.passedItems = val
+    },
+    totalPages: {
+      immediate: true,
+      handler (val, oldVal) {
+        if(val !== oldVal)
+          this.$emit('pages-change', val)
+      }
     }
   },
   methods: {
-    log (col) {
-      console.log(col)
-    },
-    checkPagination () {
-      const pages = Math.ceil((this.sortedItems.length)/ this.perPageItems) || 1
-      if(pages !== this.pages){
-        this.$emit('pages-change', pages)
-        this.pages = pages
-      }
-    },
     changeSort (name, index) {
       if(index && !this.sortable(index))
         return
@@ -354,9 +389,6 @@ export default {
         this.sorter.direction = 0
       this.sorter.name = name
     },
-    // setTableFilter (val) {
-    //   this.filter = val
-    // },
     addColumnFilter (colName, value) {
       this.$set(this.columnFilter, colName, value)
     },
@@ -375,9 +407,6 @@ export default {
                        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
                        .join(' ')
     },
-    // rowClass (item) {
-    //   return item._classes ? item._classes : ''
-    // },
     cellClass (item, colName, index) {
       let classes = []
       if(item._cellClasses && item._cellClasses[colName])
