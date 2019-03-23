6573a6253ecad50456f97f4e3ddee37583a9ee6c
woothu <32914662+woothu@users.noreply.github.com>
2019-03-23 09:16:36 +0100

    refactor: remove CCardImg component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,13 +3,11 @@ import CCard from './CCard'
 import CCardBody from './CCardBody'
 import CCardFooter from './CCardFooter'
 import CCardHeader from './CCardHeader'
-import CCardImg from './CCardImg'
 
 export {
   CCardGroup,
   CCard,
   CCardBody,
   CCardFooter,
-  CCardHeader,
-  CCardImg
+  CCardHeader
 }
