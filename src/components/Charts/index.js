8472083f08d11253ee1de443772214640e53a794
woothu <32914662+woothu@users.noreply.github.com>
2019-04-12 14:35:42 +0200

    refactor: move charts to coreui-vue-chartjs library

--- src/components/Charts/index.js
+++
@@ -1,7 +0,0 @@
-import CSimpleBarChart from './CSimpleBarChart'
-import CSimpleLineChart from './CSimpleLineChart'
-import CSimplePointedChart from './CSimplePointedChart'
-
-export {
-  CSimpleBarChart, CSimpleLineChart, CSimplePointedChart
-}
