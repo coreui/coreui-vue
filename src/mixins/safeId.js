11f56ad8ad42204c21584a3c12f92a5fa03a0b0a
woothu <32914662+woothu@users.noreply.github.com>
2019-07-05 16:10:54 +0200

    refactor: refactor safeId mixin

--- src/mixins/safeId.js
+++ src/mixins/safeId.js
@@ -1,15 +1,15 @@
 export default{
   computed: {
     safeId () {
-      if(this.id || this.$attrs.id)
+      if (this.id || this.$attrs.id) {
         return this.id || this.$attrs.id
+      }
       return generateSafeId()
     }
   }
 }
 
 function generateSafeId () {
-  const key = Math.random().toString(36).replace('0.', '') +
-              (new Date()).getTime().toString(36)
-  return '__safe_id__' + key
+  const key = Math.random().toString(36).replace('0.', '')
+  return '_safe_id_' + key
 }
