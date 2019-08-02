04c83fd85beb00cd15b90530cdd62493ddadbffd
woothu <32914662+woothu@users.noreply.github.com>
2019-08-02 11:04:52 +0200

    fix: CTable: fix pretify name function

--- src/components/Table/CTable.vue
+++ src/components/Table/CTable.vue
@@ -435,9 +435,12 @@ export default {
         input.value = ''
     },
     pretifyName (name) {
-      return name.replace(/[-_]/g, ' ').split(' ').map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1)
-      }).join(' ')
+      return name.replace(/[-_.]/g, ' ')
+        .replace(/ +/g, ' ')
+        .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
     },
     cellClass (item, colName, index) {
       let classes = []
