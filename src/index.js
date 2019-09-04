fb3246dd4ac5b1a71557c7e75fc990539d774330
woothu <32914662+woothu@users.noreply.github.com>
2019-09-04 13:42:31 +0200

    feat: add remove option to library plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import * as Components from './components'
 import * as Directives from './directives'
 
+const removeKeysFromObject = (object, keys) => {
+  return Object.entries(object).reduce((obj, [key, value]) => {
+    if (!keys.includes(key) && !keys.includes(value.name)) {
+      obj[key] = value
+    }
+    return obj
+  }, {})
+}
+
+
 const CoreuiVue = {
-  install (Vue) {
-    for (let plugin in Components) {
+  install (Vue, options) {
+    let pluginComponents = Components
+    let pluginDirectives = Directives
+
+    const toRemove = options && options.remove ? options.remove : null
+    if (toRemove && Array.isArray(toRemove)) {
+      pluginComponents = removeKeysFromObject(Components, toRemove)
+      pluginDirectives = removeKeysFromObject(Directives, toRemove)        
+    }
+
+    for (let plugin in pluginComponents) {
       Vue.component(plugin, Components[plugin])
     }
-    for (let directive in Directives) {
+    for (let directive in pluginDirectives) {
       Vue.directive(directive, Directives[directive])
     }
   }
