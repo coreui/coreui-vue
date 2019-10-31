282387ebd22a4499cfdd8023269be84a86f5c5bd
woothu <32914662+woothu@users.noreply.github.com>
2019-10-31 12:43:30 +0100

    refactor: CHeaderNavItem: delete link rendered by default

--- src/components/header/CHeaderNavItem.vue
+++ src/components/header/CHeaderNavItem.vue
@@ -1,23 +1,11 @@
 <template>
   <li class="c-header-nav-item">
-    <slot name="link">
-      <CLink
-        class="c-header-nav-link"
-        v-bind="$props"
-      >
-        <slot></slot>
-      </CLink>
-    </slot>
+    <slot></slot>
   </li>
 </template>
 
 <script>
-import CLink, { props } from '../link/CLink'
 export default {
-  name: 'CHeaderNavItem',
-  components: {
-    CLink
-  },
-  props
+  name: 'CHeaderNavItem'
 }
 </script>
