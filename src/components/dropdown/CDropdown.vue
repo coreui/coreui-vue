8228f2ed764c299f14f9f6eb91b5fffa50dd56d0
woothu <32914662+woothu@users.noreply.github.com>
2020-02-05 11:21:35 +0100

    chore: update dependencies, small refactors

--- src/components/dropdown/CDropdown.vue
+++ src/components/dropdown/CDropdown.vue
@@ -162,8 +162,8 @@ export default {
             name: 'preventOverflow',
             options: {
               padding: 10
-            },
-          },
+            }
+          }
         ]
       }
     },
