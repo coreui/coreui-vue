0492be948091b81eb6ec34322096ff60b35bc58d
woothu <32914662+woothu@users.noreply.github.com>
2020-04-07 20:27:14 +0200

    fix: CDropdownItem: fix closing dropdown when item is router link #79

--- src/components/dropdown/CDropdownItem.vue
+++ src/components/dropdown/CDropdownItem.vue
@@ -5,21 +5,30 @@ export default {
   name: 'CDropdownItem',
   props: propsFactory(),
   render (h) {
+    const data = {
+      props: this._props,
+      staticClass: 'dropdown-item',
+      attrs: { role: 'menuitem' },
+      on: {
+        //listeners added, due to component was previously functional for compatibility
+        ...this.$listeners,
+        click: (e) => {
+          this.$parent.$emit('dropdown-close')
+          this.$emit('click', e)
+        }
+      }
+    }
+    if (this.to) {
+      data.nativeOn = {
+        click: (e) => {
+          this.$parent.$emit('dropdown-close')
+          this.$emit('click', e)
+        }
+      }
+    }
     return h(
       CLink,
-      {
-        props: this._props,
-        staticClass: 'dropdown-item',
-        attrs: { role: 'menuitem' },
-        on: {
-          //listeners added, due to component was previously functional for compatibility
-          ...this.$listeners,
-          click: (e) => {
-            this.$parent.$emit('dropdown-close')
-            this.$emit('click', e)
-          }
-        }
-      },
+      data,
       this.$slots.default
     )
   }
