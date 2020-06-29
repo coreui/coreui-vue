1a643520251c1630a001ad3c94d67acf8f37e2e6
woothu <32914662+woothu@users.noreply.github.com>
2020-06-29 13:35:10 +0200

    feat: add CListGroupItemDivider wrapper component

---
+++ src/components/list-group/CListGroupItemDivider.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="list-group-item-divider"></div>
+</template>
+
+<script>
+export default {
+  name: 'CListGroupItemDivider'
+}
+</script>
--- src/components/list-group/index.js
+++ src/components/list-group/index.js
@@ -1,7 +1,9 @@
 import CListGroup from './CListGroup'
 import CListGroupItem from './CListGroupItem'
+import CListGroupItemDivider from './CListGroupItemDivider'
 
 export {
   CListGroup,
-  CListGroupItem
+  CListGroupItem,
+  CListGroupItemDivider
 }
