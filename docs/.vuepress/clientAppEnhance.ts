8921707753568e4acce647528c8f72bd1a2b2121
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-08 22:33:41 +0200

    docs: update icons

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -1,9 +1,19 @@
 import type { ClientAppEnhance } from '@vuepress/client'
+import { CIcon } from '@coreui/icons-vue'
 import CoreuiVue from '../../src/'
 import '@coreui/coreui/scss/coreui.scss'
 
+import { cilPuzzle, cilSpeedometer } from '@coreui/icons'
+
+export const icons = {
+  cilPuzzle,
+  cilSpeedometer,
+}
+
 const clientAppEnhance: ClientAppEnhance = ({ app }) => {
   app.use(CoreuiVue)
+  app.provide('icons', icons)
+  app.component('CIcon', CIcon)
 }
 
 export default clientAppEnhance
