0bc9c7fba8ae4900d27f17586bade7694797e6ed
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-27 00:08:59 +0200

    feat(CInputGroupText): add dynamic component support

--- src/components/form/CInputGroupText.ts
+++ src/components/form/CInputGroupText.ts
@@ -2,8 +2,20 @@ import { defineComponent, h } from 'vue'
 
 const CInputGroupText = defineComponent({
   name: 'CInputGroupText',
-  setup(_, { slots }) {
-    return () => h('span', { class: 'input-group-text' }, slots.default && slots.default())
+  props: {
+    /**
+     * Component used for the root node. Either a string to use a HTML element or a component.
+     *
+     * @default 'span'
+     */
+    component: {
+      type: String,
+      required: false,
+      default: 'span',
+    },
+  },
+  setup(props, { slots }) {
+    return () => h(props.component, { class: 'input-group-text' }, slots.default && slots.default())
   },
 })
 
