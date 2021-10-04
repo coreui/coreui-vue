3606a69c156cf2af7fcbd79b04230166a51f9e89
Łukasz Holeczek <lukasz@holeczek.pl>
2021-10-04 21:50:10 +0200

    refactor: update directives

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,5 +1,4 @@
 import { CTooltip } from './CTooltip'
 import { CPopover } from './CPopover'
-import CClickaway from './CClickaway'
 
-export { CTooltip, CPopover, CClickaway }
+export { CTooltip, CPopover }
