3abca642cb143ecacb75bbeba41c2f002730ba4b
Łukasz Holeczek <lukasz@holeczek.pl>
2022-01-22 16:34:13 +0100

    docs: update theme

--- packages/docs/.vuepress/theme-coreui/src/client/clientAppEnhance.ts
+++ packages/docs/.vuepress/theme-coreui/src/client/clientAppEnhance.ts
@@ -2,7 +2,7 @@ import { h } from 'vue'
 import { defineClientAppEnhance } from '@vuepress/client'
 import CodeGroup from './components/global/CodeGroup'
 import CodeGroupItem from './components/global/CodeGroupItem.vue'
-import OutboundLink from './components/global/OutboundLink.vue'
+// import OutboundLink from './components/global/OutboundLink.vue'
 import { useScrollPromise } from './composables'
 
 import './styles/index.scss'
@@ -11,12 +11,6 @@ export default defineClientAppEnhance(({ app, router }) => {
   app.component('CodeGroup', CodeGroup)
   app.component('CodeGroupItem', CodeGroupItem)
 
-  // unregister the built-in `<OutboundLink>` to avoid warning
-  delete app._context.components.OutboundLink
-  // override the built-in `<OutboundLink>`
-  app.component('OutboundLink', OutboundLink)
-
-  // compat with @vuepress/plugin-docsearch and @vuepress/plugin-search
   app.component('NavbarSearch', () => {
     const SearchComponent = app.component('Docsearch') || app.component('SearchBox')
     if (SearchComponent) {
