c17453fc58b8c6b993abe68d67354fa399dafb45
Łukasz Holeczek <lukasz@holeczek.pl>
2023-01-17 00:58:20 +0100

    refactor(CTableCaption): remove empty props object

--- packages/coreui-vue/src/components/table/CTableCaption.ts
+++ packages/coreui-vue/src/components/table/CTableCaption.ts
@@ -2,7 +2,6 @@ import { defineComponent, h } from 'vue'
 
 const CTableCaption = defineComponent({
   name: 'CTableCaption',
-  props: {},
   setup(_, { slots }) {
     return () => h('caption', {}, slots.default && slots.default())
   },
