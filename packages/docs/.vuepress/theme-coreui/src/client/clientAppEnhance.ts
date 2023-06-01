1bd205e2a05ed5c66c7f0f2bab0b613656dc1c25
mrholek <lukasz@holeczek.pl>
2023-06-01 19:12:00 +0200

    docs: clean-up

--- packages/docs/.vuepress/theme-coreui/src/client/clientAppEnhance.ts
+++ packages/docs/.vuepress/theme-coreui/src/client/clientAppEnhance.ts
@@ -1,24 +1,9 @@
-import { h } from 'vue'
 import { defineClientAppEnhance } from '@vuepress/client'
-import CodeGroup from './components/global/CodeGroup'
-import CodeGroupItem from './components/global/CodeGroupItem.vue'
-// import OutboundLink from './components/global/OutboundLink.vue'
 import { useScrollPromise } from './composables'
 
 import './styles/index.scss'
 
 export default defineClientAppEnhance(({ app, router }) => {
-  app.component('CodeGroup', CodeGroup)
-  app.component('CodeGroupItem', CodeGroupItem)
-
-  app.component('NavbarSearch', () => {
-    const SearchComponent = app.component('Docsearch') || app.component('SearchBox')
-    if (SearchComponent) {
-      return h(SearchComponent)
-    }
-    return null
-  })
-
   // handle scrollBehavior with transition
   const scrollBehavior = router.options.scrollBehavior!
   ;(router.options.scrollBehavior = async (...args) => {
