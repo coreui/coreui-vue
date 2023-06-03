194db403a4d2c8ab4e1337e69f728a5cf65f7f1d
mrholek <lukasz@holeczek.pl>
2023-06-03 16:04:03 +0200

    chore: update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,23 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    'plugin:unicorn/recommended',
+    '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-dom-node-append': 'off',
+    'unicorn/prefer-export-from': 'off',
+    'unicorn/prefer-query-selector': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'vue/require-default-prop': 'off',
   },
   overrides: [
     {
