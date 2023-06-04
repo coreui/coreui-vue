c62b4c39b03921b7e88b7e9048fea72203c15b01
mrholek <lukasz@holeczek.pl>
2023-06-04 10:59:17 +0200

    docs: update theme

--- packages/docs/.vuepress/client.ts
+++ packages/docs/.vuepress/client.ts
@@ -1,7 +1,7 @@
 import { defineClientConfig } from '@vuepress/client'
 import { CIcon } from '@coreui/icons-vue'
 import CChartPlugin from '@coreui/vue-chartjs/src/'
-import CoreuiVue from '@coreui/vue/src'
+import CoreuiVue from '@coreui/vue/src/'
 import '@coreui/coreui/scss/coreui.scss'
 import '@coreui/chartjs/scss/coreui-chartjs.scss'
 
