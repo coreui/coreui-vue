9cbf88cab5eacd7cd8211ab6aa601986bc23118b
mrholek <lukasz@holeczek.pl>
2026-03-24 12:27:32 +0100

    docs: update banner

--- packages/docs/.vuepress/src/client/components/Banner.vue
+++ packages/docs/.vuepress/src/client/components/Banner.vue
@@ -1,48 +1,48 @@
 <script setup lang="ts">
-import { usePageFrontmatter } from '@vuepress/client'
+import { usePageFrontmatter, withBase } from '@vuepress/client'
 import type { DefaultThemePageFrontmatter } from '../../shared'
 
 const frontmatter = usePageFrontmatter<DefaultThemePageFrontmatter>()
+const pre_release = frontmatter.value.pre_release
 const pro_component = frontmatter.value.pro_component
 </script>
 
 <template>
   <div
     v-if="pro_component"
-    class="bg-danger bg-opacity-10 border-start border-start-5 border-start-danger p-4 pb-3 mb-5"
+    class="bg-info bg-opacity-10 border border-2 border-info rounded p-3 px-xl-4 mb-5"
   >
-    <h3 class="mb-4">CoreUI PRO Component</h3>
-    <p>
-      To use this component you must have a CoreUI PRO license. Buy the
-      <a href="https://coreui.io/pricing/?framework=vue&docs=coreui-banner-pro">CoreUI PRO</a>
-      and get access to all PRO components, features, templates, and dedicated support.
-    </p>
+    <div class="row d-flex align-items-center flex-xl-nowrap">
+      <div class="col-xl-auto col-12 d-none d-xl-block p-0">
+        <img
+          :src="withBase('/images/hex_vue.png')"
+          width="160px"
+          height="160px"
+          alt="CoreUI PRO hexagon"
+        />
+      </div>
+      <div class="col-md col-12 px-lg-4">
+        This component is part of CoreUI PRO – a powerful UI library with over 250 components and
+        25+ templates, designed to help you build modern, responsive apps faster. Fully compatible
+        with Angular, Bootstrap, React.js, and Vue.js.
+      </div>
+      <div class="col-md-auto col-12 mt-3 mt-lg-0">
+        <a
+          class="btn btn-danger text-nowrap text-white"
+          href="https://coreui.io/pricing/?framework=vue&src=vue-docs&cta=banner"
+          >Get CoreUI PRO</a
+        >
+      </div>
+    </div>
   </div>
   <div
-    v-else
-    class="bg-info bg-opacity-10 border-start border-start-5 border-start-info p-4 pb-3 mb-5"
+    v-if="pre_release"
+    class="bg-warning d-none d-lg-block bg-opacity-10 border-start border-start-5 border-start-warning p-4 pb-3 mb-5"
   >
-    <h3 class="mb-4">Support CoreUI Development</h3>
+    <h3 className="mb-4">Release candidate (RC)</h3>
     <p>
-      CoreUI is an MIT-licensed open source project and is completely free to use. However, the
-      amount of effort needed to maintain and develop new features for the project is not
-      sustainable without proper financial backing.
+      This component is in the Release Candidate phase and its API is considered stable. Minor
+      adjustments may still occur before the final release.
     </p>
-    <p>You can support our Open Source software development in the following ways:</p>
-    <ul>
-      <li>
-        Buy the
-        <a href="https://coreui.io/pricing/?framework=vue&docs=coreui-banner-pro">CoreUI PRO</a>,
-        and get access to PRO components, and dedicated support.
-      </li>
-      <li>
-        <a href="https://opencollective.com/coreui" target="_blank">Became a sponsor</a>, and get
-        your logo on BACKERS.md/README.md files or each site of this documentation
-      </li>
-      <li>
-        Give us a star ⭐️ on
-        <a href="https://github.com/coreui/coreui-vue" target="_blank">Github</a>.
-      </li>
-    </ul>
   </div>
 </template>
